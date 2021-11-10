14297d0a8773f47d30c323c124403273d90f7ee5
gonzalo <gonzalo_ns@live.com.ar>
2021-11-10 20:35:48 -0300

    Creacion PhotoMap

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FamilyInfoCard } from './Components/FamilyInfoCard';
 import { GoogleSignin, GoogleSigninButton, statusCodes} from '@react-native-google-signin/google-signin';
 import PictureAdder from './Components/pictureAdder';
+import PhotoMap from './Components/PhotoMap';
 
 /* https://reactnavigation.org/docs/native-stack-navigator/ */
 
@@ -67,10 +68,12 @@ function FamilyScreen({ navigation, route }) {
   }, [navigation])
 
   return (
+    // Cambiar con fotos <FlatList keyExtractor={(item) => item._id} data={usuarios} renderItem={ ({item}) => <TouchableHighlight onPress={() => navigation.navigate('Map',item._id)}><FamilyInfoCard item={item}/></TouchableHighlight>} />
     <View>
       <Text>Aca van las fotos de la familia {id}</Text>
       <Image source={{uri: 'https://drive.google.com/thumbnail?id=1bDYTk5uvJTE3_bvTQ1TjnzmhZ3Va0Xib'}} alt={"Doesn't work"}
        style={{width: 400, height: 400}} />
+       
        <PictureAdder familyid = {id}></PictureAdder>
     </View>
   );
@@ -93,12 +96,55 @@ function FamiliesScreen ({navigation}) {
   }
 
   return (
+    //<FlatList keyExtractor={(item) => item._id} data={usuarios} renderItem={ ({item}) => <TouchableHighlight onPress={() => navigation.navigate('Family', {id: item._id})}><FamilyInfoCard item={item}/></TouchableHighlight>} />
     <View style={styles.container_style}>
-      <FlatList keyExtractor={(item) => item._id} data={usuarios} renderItem={ ({item}) => <TouchableHighlight onPress={() => navigation.navigate('Family', {id: item._id})}><FamilyInfoCard item={item}/></TouchableHighlight>} />
+      
+      <FlatList keyExtractor={(item) => item._id} data={usuarios} renderItem={ ({item}) => <TouchableHighlight onPress={() => navigation.navigate('Map',item._id)}><FamilyInfoCard item={item}/></TouchableHighlight>} />
     </View>
   );
 };
 
+
+
+function MapScreen ({navigation,route}){
+
+  const { id } = route.params;
+
+  const [ latitude, setLatitude ] = useState(0);
+  const [ longitude, setLongitude ] = useState(0);
+  
+    
+
+
+
+  const obtenerDatos = async () => {
+    const data = await fetch("https://modulo-sanitario-imagenes-db.herokuapp.com/families/61848ce34b6dd700161057cb")
+    const response = await data.json()
+    setLatitude(response.coordinates.latitude)
+    setLongitude(response.coordinates.longitude)
+    //console.log(information)
+    
+    
+  }
+
+  useEffect( () => {
+    obtenerDatos()
+  },[])
+
+  return(
+    <View>
+
+    <PhotoMap latitude={latitude} longitude={longitude}></PhotoMap>
+
+    </View>
+
+
+  );
+
+
+  
+
+}
 const styles = StyleSheet.create({
   container_style: {
     flex: 1,
@@ -120,6 +166,7 @@ function MyStack() {
       <Stack.Screen name="Iniciar Sesion" component={GoogleLogIn} />
       <Stack.Screen name="Families" component={FamiliesScreen} />
       <Stack.Screen name="Family" component={FamilyScreen} />
+      <Stack.Screen name="Map" component={MapScreen} />
     </Stack.Navigator>
   );
 }
---
+++ Components/PhotoMap.js
@@ -0,0 +1,40 @@
+import React,{Component} from "react";
+import { render } from "react-dom";
+import{Platform, StyleSheet,Text,View} from "react-native";
+import MapView, {Marker} from "react-native-maps";
+import Geolocation from '@react-native-community/geolocation';
+
+
+
+export default class PhotoMap extends Component{
+ 
+    render(){
+        return(
+
+            <View style= {styles.container}>
+                <MapView 
+                    style={styles.map}
+                    region={{
+                        latitude:this.props.latitude,
+                        longitude:this.props.longitude,
+                        latitudeDelta: 0.015,
+                        longitudeDelta:0.0121,
+                    }}
+                >
+                <Marker coordinate={{latitude:this.props.latitude, longitude:this.props.longitude}}></Marker>
+              
+                </MapView>
+
+            </View>
+        );
+    }
+
+}
+const styles= StyleSheet.create({
+    container:{
+        width: '100%', height: '100%'
+    },
+    map:{
+        width: '100%', height: '100%' 
+    }
+});
