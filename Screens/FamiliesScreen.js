b3707687b10c43144449c6d7880f95cae6a5d8f8
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-12-11 17:54:44 -0300

    flatlist infinito agregado

--- Screens/FamiliesScreen.js
+++ Screens/FamiliesScreen.js
@@ -1,6 +1,6 @@
 import { FamilyInfoCard } from '../Components/FamilyInfoCard';
 import React, { useEffect, useState } from 'react';
-import { Button, View, StyleSheet, Text, FlatList, Modal, Pressable, KeyboardAvoidingView, ActivityIndicator} from 'react-native';
+import { Button, View, StyleSheet, Text, FlatList, Modal, Pressable, KeyboardAvoidingView, ActivityIndicator, useColorScheme} from 'react-native';
 import { Dimensions } from 'react-native';
 import asyncStorageHelper from '../Helpers/asyncStorageHelper'
 import { TextInput, Title } from 'react-native-paper';
@@ -9,14 +9,15 @@ import Icon from 'react-native-vector-icons/FontAwesome5';
 import { GoogleSignin} from '@react-native-google-signin/google-signin';
 
 import { LogBox } from 'react-native';
-LogBox.ignoreAllLogs();
+import { CurrentRenderContext } from '@react-navigation/native';
+//LogBox.ignoreAllLogs();
 
 const { width, height } = Dimensions.get('window');
 const SCREEN_WIDTH = width < height ? width : height;
 
 
 export default function FamiliesScreen ({ navigation }) {
-  const [ usuarios, setUsuarios ] = useState();
+  const [ usuarios, setUsuarios ] = useState([]);
   const [token, setToken] = useState();
   const [apellido, setApellido] = useState('');
   const [barrio, setBarrio] = useState('');
@@ -24,12 +25,14 @@ export default function FamiliesScreen ({ navigation }) {
   const [apellidoHolder, setApellidoHolder] = useState('');
   const [barrioHolder, setBarrioHolder] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
-
+  const [currentPage, setCurrentPage] = useState(1)
+  const [isFetching, setIsFetching] = useState(false)
+  const [shouldClean, setShouldClean] = useState(true)
 
   useEffect( () => {
     console.log('useEffect')
     obtenerDatos()
-  }, [apellido, barrio])
+  }, [apellido, barrio, currentPage])
 
 
   navigation.setOptions({
@@ -40,12 +43,10 @@ export default function FamiliesScreen ({ navigation }) {
       return (  <Icon size={30} style={{marginRight:20}} onPress={() => {asyncStorageHelper.limpiarToken();GoogleSignin.signOut();navigation.goBack()}} name='door-open' />)
     },
   })
-  
-  /*headerRight: () => (<View>
-    <Text onPress={() => setModalVisible(true)}>Filtrar</Text>
-    <Text>Cerrar Sesión</Text></View>),*/
 
   const obtenerDatos = async () => {
+    setIsFetching(true)
+
     jwt = await asyncStorageHelper.obtenerToken()
     //console.log(jwt)
     https_options = { 
@@ -55,22 +56,37 @@ export default function FamiliesScreen ({ navigation }) {
       })
     }
 
-    url = "http://modulo-backoffice.herokuapp.com/families/obtain-families?"
+    let url = "http://modulo-backoffice.herokuapp.com/families/obtain-families?limit=4&page="+currentPage
 
     if(apellido){
-      url += "apellido="+apellido+"&"
+      url += "&apellido="+apellido
     }
     
     if(barrio){
-      url += "barrio="+barrio+"&"
+      url += "&barrio="+barrio
     }
 
     console.log(url)
     const data = await fetch(url, https_options)
     const users = await data.json()
-    setUsuarios(users.results)
+
+    console.log("Usuarios Obtenidos:", users.results)
+
+    if(shouldClean){
+      console.log("Se limpia usuarios...")
+      setUsuarios(users.results)
+      setShouldClean(false)
+    }
+    else if(users.length != 0){
+      let newUsers = [...usuarios,...users.results]
+      let uniqueUsers = newUsers.filter((v, i, a) => a.findIndex(t => (t._id === v._id)) === i)
+      console.log("Usuarios que se muestran:", uniqueUsers)
+      setUsuarios(uniqueUsers)
+    }
+
     setToken(jwt)
     setLoading(false)
+    setIsFetching(false)
   }
 
   /*
@@ -82,6 +98,10 @@ export default function FamiliesScreen ({ navigation }) {
       <FamilyInfoCard navigation={navigation} item={item} token={token}></FamilyInfoCard>
   );
 
+  const fetchMoreFamilies = async () => {
+    setCurrentPage(currentPage + 1)
+  }
+
   return (
     <View>
       <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => {setModalVisible(!modalVisible);}}>
@@ -92,17 +112,21 @@ export default function FamiliesScreen ({ navigation }) {
             <TextInput style={styles2.textInput} type="text" onChangeText={(text) => setApellidoHolder(text)} value={apellidoHolder} placeholder={"Ingrese un apellido..."} />
             <TextInput style={styles2.textInput} type="text" onChangeText={(text) => setBarrioHolder(text)} value={barrioHolder} placeholder={"Ingrese un barrio..."} />
             <Pressable style={[styles2.button, styles2.buttonClose]} onPress={() => {
+                setShouldClean(true)
                 setApellido(apellidoHolder)
                 setBarrio(barrioHolder)
+                setCurrentPage(1)
                 setModalVisible(!modalVisible)
                 }}>
               <Text style={styles2.textStyle}>Filtrar</Text>
             </Pressable>
             <Pressable style={[styles2.buttonBorrar, styles2.buttonClose]} onPress={() => {
+                setShouldClean(true)
                 setBarrioHolder('')
                 setApellidoHolder('')
                 setApellido('')
                 setBarrio('')
+                setCurrentPage(1)
                 setModalVisible(!modalVisible)
                 }}>
               <Text style={styles2.textStyle}>Borrar</Text>
@@ -127,25 +151,28 @@ export default function FamiliesScreen ({ navigation }) {
           />
         ) : (
 
-          <FlatList data={usuarios} ListEmptyComponent={
-      
-      
-            <View style={{
-              height: height,
-              width: width,
-              backgroundColor:"transparent",
-              alignItems: "center"
-            }}>
-              <Icon name="exclamation-circle" size={100} style={{marginTop:250}} color="#A00"/>
-              <Text style={{
-                textAlign: "center",
-                marginTop: 20
-              }}>No hay familias que coincidan con los parametros de búsqueda</Text>
-            </View>
-          
-          
-          } renderItem={renderFamily} keyExtractor={(item) => item._id} />
-
+          <FlatList
+            refreshing={isFetching}
+            data={usuarios} 
+            ListEmptyComponent={
+              <View style={{
+                height: height,
+                width: width,
+                backgroundColor:"transparent",
+                alignItems: "center"
+              }}>
+                <Icon name="exclamation-circle" size={100} style={{marginTop:250}} color="#A00"/>
+                <Text style={{
+                  textAlign: "center",
+                  marginTop: 20
+                }}>No hay familias que coincidan con los parametros de búsqueda</Text>
+              </View>
+            }
+            renderItem={renderFamily}
+            onEndReached = {fetchMoreFamilies}
+            onEndReachedThreshold={0.5}
+            keyExtractor={(item, index) => item._id.toString()}
+            />
         )}
 
     </View>
