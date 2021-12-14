038b823ba4a1bc47d219c07c972f9d05c6709ca5
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-12-14 15:41:25 -0300

    comentarios de asyncstorage

--- Helpers/asyncStorageHelper.js
+++ Helpers/asyncStorageHelper.js
@@ -1,5 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+/*
+
+AsyncStorage es una base de datos descriptado, asincrona, persistente
+y de tipo key-value.
+
+Hay solamente 3 metodos importantes:
+
+- getItem(): Dada una key (un string), devuelve una promise. De esta manera
+conseguimos la value para la key que pasamos.
+- setItem(): Setea un valor para una key determinada. Retorna tambien una
+promise.
+- removeItem(): Borra el item de una key determinada. Retornar tambien una
+promise.
+
+En todas podemos agregar una funcion de callback en caso de que haya error.
+En caso de que queramos guardar un json tenemos que usar JSON.stringify para
+guardarlo como string.
+
+*/
+
 const guardarToken = (token) => {
     try {
         AsyncStorage.setItem('@storage_token', token)
